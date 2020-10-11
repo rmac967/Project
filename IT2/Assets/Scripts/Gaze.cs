33a6f278d6a098158681da54678abd52c02e9212
rmac967 <69334774+rmac967@users.noreply.github.com>
2020-10-11 23:14:35 +1300

    Iteration 2

---
+++ IT2/Assets/Scripts/Gaze.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour {
+    List<InfoBeh> infos = new List<InfoBeh>();
+    
+
+    void Start() {
+        infos = FindObjectsOfType<InfoBeh>().ToList();
+
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("hasInfo")){
+                OpenInfo(go.GetComponent<InfoBeh>());
+            }
+        }
+        else
+        {
+            CloseAll();
+        }
+    }
+
+    void OpenInfo(InfoBeh desiredInfo)
+    {
+        foreach (InfoBeh info in infos)
+        {
+            if (info == desiredInfo)
+            {
+                info.OpenInfo();
+            }
+            else
+            {
+                info.CloseInfo();
+            }
+        }
+    }
+
+    void CloseAll()
+    {
+        foreach (InfoBeh info in infos)
+        {
+            info.CloseInfo();
+        }
+    }
+}
---
+++ IT2/Assets/Scripts/Gaze2.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Gaze2 : MonoBehaviour
+{
+    List<InfoBeh2> infos = new List<InfoBeh2>();
+
+
+    void Start()
+    {
+        infos = FindObjectsOfType<InfoBeh2>().ToList();
+
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("hasinfo2"))
+            {
+                OpenInfo(go.GetComponent<InfoBeh2>());
+            }
+        }
+        else
+        {
+            CloseAll();
+        }
+    }
+
+    void OpenInfo(InfoBeh2 desiredInfo)
+    {
+        foreach (InfoBeh2 info in infos)
+        {
+            if (info == desiredInfo)
+            {
+                info.OpenInfo();
+            }
+            else
+            {
+                info.CloseInfo();
+            }
+        }
+    }
+
+    void CloseAll()
+    {
+        foreach (InfoBeh2 info in infos)
+        {
+            info.CloseInfo();
+        }
+    }
+}
